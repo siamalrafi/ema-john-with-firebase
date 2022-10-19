72eeebf66571c1de575040af0dcac242930b4df2
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-10-19 11:13:19 +0600

    This is the next part

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SignUp.css';
 
 const SignUp = () => {
+
+    const [error, setError] = useState();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confrim = form.confrim.value;
+        console.log(email, password, confrim);
+
+        if (password.lenght < 6) {
+            setError('Password must be 6 characters long.')
+            return;
+        }
+        if (password !== confrim) {
+            setError('Your password did not match.');
+            return;
+        }
+
+    }
+
+
+
+
     return (
         <div className='form-container'>
             <h2 className='form-title'>This is the Sign Up</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name='email' required />
@@ -17,11 +43,12 @@ const SignUp = () => {
                 </div>
                 <div className="form-control">
                     <label htmlFor="password">Confirm Password</label>
-                    <input type="password" name='password' required />
+                    <input type="password" name='confrim' required />
                 </div>
                 <input className='btn-submit' type="submit" value='Sign Up' />
             </form>
             <p className="form-control">Already have an Account? <Link to='/login'>Login</Link></p>
+            <p className='form-control text-error'>{error}</p>
         </div>
     );
 };
